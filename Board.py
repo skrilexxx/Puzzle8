56797fba5a4ba2d38e1cfcf12f0b705c7fe7fdf5
skrilexxx <admin.nuklear@seznam.cz>
2024-03-27 17:10:58 +0100

    added solver

--- Board.py
+++ Board.py
@@ -140,12 +140,8 @@ class Board:
         else:
             return False
 
-
-
-
     
 board = Board([[8, 1, 3,],[4, 0, 2],[7, 6, 5]])
-board2 = Board([[1, 2, 3],[4, 5, 6],[8, 7, 0]])
 
 print(board.isGoal())
 print("Not in rigth place: ",board.hamming())
@@ -153,4 +149,6 @@ print("Distance: ", board.manhattan())
 board.printBoard()
 board.neighbors()
 print("Is board 1 solvable ?: ", board.isSolvable())
-print("Is board 2 solvable ?: ", board2.isSolvable())
+
+#board2 = Board([[1, 2, 3],[4, 5, 6],[8, 7, 0]])
+#print("Is board 2 solvable ?: ", board2.isSolvable())
---
+++ Solver.py
@@ -0,0 +1,20 @@
+from Board import *
+
+class Solver:
+
+    def __init__(self, input, goal):
+        self.input = input
+        self.goal = goal
+        self.board = Board(input)
+
+    def solver(self, boardInitial):
+        pass
+
+    def moves(self):
+        pass
+
+    def solution(self):
+        pass
+
+
+test1 = Solver([[8, 1, 3],[4, 0, 2],[7, 6, 5]], [[1, 2, 3],[4, 5, 6],[7, 8, 0]])
